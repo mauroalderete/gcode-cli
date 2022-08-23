38de338dc2d41ffd190b84030f6fca477490ae09
Mauro Alderete <alderete.mauro@gmail.com>
2022-08-23 13:12:19 -0300

    feat: add cobra-cli init result

---
+++ cmd/root.go
@@ -0,0 +1,36 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "gcode-cli",
+	Short: "Gcode command-line tool to oparate with gcode files",
+	Long: `gcode-cli is a command-line tool that help you to apply
+masive operations to your gcode files like skew corrections, translations or chekcsum integrity.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gcode-cli.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
