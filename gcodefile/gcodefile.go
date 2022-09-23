f90e62b92d3aa364b630e913bb0c9a32c3be9386
Mauro Alderete <alderete.mauro@gmail.com>
2022-09-23 14:40:19 -0300

    test: add unit test to instance a new gcodefile object

--- gcodefile/gcodefile.go
+++ gcodefile/gcodefile.go
@@ -1,21 +1,48 @@
 package gcodefile
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"os"
+	"strings"
 
-	"github.com/mauroalderete/gcode-core/gcode"
+	"github.com/mauroalderete/gcode-core/block/gcodeblock"
 )
 
 type GcodeFile struct {
 	source io.Reader
-	gcodes []gcode.Gcoder
+	gcodes []*gcodeblock.GcodeBlock
 }
 
-func (gf *GcodeFile) Gcodes() []gcode.Gcoder {
+func (gf *GcodeFile) Gcodes() []*gcodeblock.GcodeBlock {
 	return gf.gcodes
 }
 
 func (gf *GcodeFile) Refresh() error {
+
+	scanner := bufio.NewScanner(gf.source)
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		//line is empty
+		if len(line) == 0 {
+			continue
+		}
+
+		//line is a comment
+		if strings.HasPrefix(line, ";") {
+			continue
+		}
+
+		block, err := gcodeblock.Parse(line)
+		if err != nil {
+			return fmt.Errorf("%v", err)
+		}
+
+		gf.gcodes = append(gf.gcodes, block)
+	}
+
 	return nil
 }
 
@@ -27,10 +54,27 @@ func (gf *GcodeFile) SaveFile(path string) error {
 	return nil
 }
 
-func NewFromFile(source string) (*GcodeFile, error) {
-	return nil, nil
+func NewFromFile(path string) (*GcodeFile, error) {
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed open file in '%s': %v", path, err)
+	}
+
+	return NewFromReader(file)
 }
 
 func NewFromReader(source io.Reader) (*GcodeFile, error) {
-	return nil, nil
+
+	gf := GcodeFile{
+		gcodes: make([]*gcodeblock.GcodeBlock, 0),
+		source: source,
+	}
+
+	err := gf.Refresh()
+	if err != nil {
+		return nil, fmt.Errorf("failed parse source: %v", err)
+	}
+
+	return &gf, nil
 }
