39841ae7d299d20fc8b6667da220699598ccdde4
Mauro Alderete <alderete.mauro@gmail.com>
2022-09-23 16:40:41 -0300

    doc: add some doc to gcodefile

--- gcodefile/gcodefile.go
+++ gcodefile/gcodefile.go
@@ -10,15 +10,35 @@ import (
 	"github.com/mauroalderete/gcode-core/block/gcodeblock"
 )
 
+// GcodeFile model a file or another type source that contain a 3D object written in gcode format.
+//
+// GcodeFile implements basic methods to handle the file, open, read it and save.
+// Exposes the list of Gcodes that define the 3d object to process the model or apply changes.
+// Series of methods allows Update the source of model and save it as a file.
 type GcodeFile struct {
 	source io.Reader
 	gcodes []*gcodeblock.GcodeBlock
 }
 
+// Gcodes returns the lis of blocks found in the source, after parse it, as a list of [gcodeblock.GcodeBlock]
 func (gf *GcodeFile) Gcodes() []*gcodeblock.GcodeBlock {
 	return gf.gcodes
 }
 
+// Source returns the source as [io.Reader] that store the blocks written as plain text line by line.
+func (gf *GcodeFile) Source() io.Reader {
+	return gf.source
+}
+
+// Refresh parse the gcode source loaded to get a list of [gcodeblock.GcodeBlock]
+//
+// Read the source content line by line to get his [gcodeblock.GcodeBlock] representation.
+//
+// If the source is empty or only contains comments then Refresh parse a list empty.
+//
+// If the source is nil then refresh returns an error.
+//
+// If the source contain an invalid block then Refresh returns an error.
 func (gf *GcodeFile) Refresh() error {
 
 	scanner := bufio.NewScanner(gf.source)
@@ -46,14 +66,16 @@ func (gf *GcodeFile) Refresh() error {
 	return nil
 }
 
-func (gf *GcodeFile) Update() (io.Reader, error) {
-	return nil, nil
+// Update takes the last state of the blocks in the list stored and updates the source with the changes made to this moment.
+func (gf *GcodeFile) Update() error {
+	return nil
 }
 
 func (gf *GcodeFile) SaveFile(path string) error {
 	return nil
 }
 
+// NewFromFile returns a new instance of [GcodeFile] from a file with the gcode blocks contented.
 func NewFromFile(path string) (*GcodeFile, error) {
 
 	file, err := os.Open(path)
@@ -64,6 +86,7 @@ func NewFromFile(path string) (*GcodeFile, error) {
 	return NewFromReader(file)
 }
 
+// NewFromReader returns a new instance of [GcodeFile] from an [io.Reader] object with the gcode blocks contented.
 func NewFromReader(source io.Reader) (*GcodeFile, error) {
 
 	gf := GcodeFile{
