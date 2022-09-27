3a370199fe22def9ab03ca40f93a945b6a985ee8
Mauro Alderete <alderete.mauro@gmail.com>
2022-09-27 16:47:19 -0300

    feat: skew command on xy plane with unit tests, save file and many input meassures

--- cmd/skew/xy/xy.go
+++ cmd/skew/xy/xy.go
@@ -4,8 +4,10 @@ package xy
 import (
 	"fmt"
 	"io"
+	"math"
 	"os"
 
+	"github.com/mauroalderete/gcode-cli/gcodefile"
 	"github.com/mauroalderete/gcode-cli/skewer"
 	"github.com/spf13/cobra"
 )
@@ -16,18 +18,22 @@ type flag[T interface{}] struct {
 	value T
 }
 
+type skewXYFlags struct {
+	degree flag[float32]
+	radian flag[float32]
+	ratio  flag[float32]
+	output flag[string]
+}
+
 // NewSkewCommand creates an instance of *cobra.Command with the behaviour
 func NewXYCommand() *cobra.Command {
 
 	// Initializes flags bindable
-	options := struct {
-		degree flag[float32]
-		radian flag[float32]
-		ratio  flag[float32]
-	}{
+	flags := skewXYFlags{
 		degree: flag[float32]{name: "degree", value: 0},
 		radian: flag[float32]{name: "radian", value: 0},
 		ratio:  flag[float32]{name: "ratio", value: 0},
+		output: flag[string]{name: "output", value: ""},
 	}
 
 	// Defines the command describe
@@ -41,49 +47,98 @@ With no FILE, or when FILE is -, read standard input.
 	}
 
 	// Load the falgs using options instance to binding the values
-	// Load the falgs using options instance to binding the values
-	cmd.Flags().Float32Var(&options.degree.value, options.degree.name, options.degree.value, "Skew value in degrees")
-	cmd.Flags().Float32Var(&options.radian.value, options.radian.name, options.radian.value, "Skew value in radians")
-	cmd.Flags().Float32Var(&options.ratio.value, options.ratio.name, options.ratio.value, "Skew value in ratio")
+	cmd.Flags().Float32Var(&flags.degree.value, flags.degree.name, flags.degree.value, "Skew value in degrees")
+	cmd.Flags().Float32Var(&flags.radian.value, flags.radian.name, flags.radian.value, "Skew value in radians")
+	cmd.Flags().Float32Var(&flags.ratio.value, flags.ratio.name, flags.ratio.value, "Skew value in ratio")
+	cmd.Flags().StringVar(&flags.output.value, flags.output.name, flags.output.value, "File output to store the gcode fixed")
 
 	cmd.MarkFlagsMutuallyExclusive(
-		options.degree.name,
-		options.radian.name,
-		options.ratio.name,
+		flags.degree.name,
+		flags.radian.name,
+		flags.ratio.name,
 	)
 
-	// Loads the handler of command.
-	// Consumes options to determine the correct behaivour
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
+
+		ratio, err := getRatio(cmd, flags)
+		if err != nil {
+			return err
+		}
 
 		input, err := getInput(cmd.InOrStdin(), args)
 		if err != nil {
 			return err
 		}
 
-		// gcode file struct
-		//gets origin of source (file or stdin) => io.Reader
-		//	probably two news
-		//parses io.Reader => []gcode.Gcoder
-		//
-		// apply changes to each gcoder that needed it
-		//
-		//generates new file from []gcode.Gcoder
-		//generates maintaining comments??
-		//
-		//save new file / flag output file
-
-		_, err = skewer.New(func(c skewer.Configurer) error {
-			return c.SetSource(nil)
-		})
+		gfile, err := gcodefile.NewFromReader(input)
 		if err != nil {
-			return fmt.Errorf("failed to instanciate Skewer: %v", err)
+			return fmt.Errorf("failed instance a gcode file")
+		}
+
+		blocks := gfile.Gcodes()
+
+		blocksSkewed, err := skewer.SkewXY(ratio, blocks)
+		if err != nil {
+			return fmt.Errorf("failed apply skew transformation: %v", err)
+		}
+
+		gfile.Update(blocksSkewed)
+
+		if flags.output.value == "" {
+			toPrint, err := io.ReadAll(gfile.Source())
+			if err != nil {
+				return fmt.Errorf("failed print gcode source transformated: %v", err)
+			}
+			cmd.OutOrStdout().Write(toPrint)
+			return nil
+		}
+
+		err = gfile.SaveFile(flags.output.value)
+		if err != nil {
+			return fmt.Errorf("failed save gcode source transformated: %v", err)
 		}
 
 		return nil
 	}
 
 	return cmd
+}
+
+func getRatio(cmd *cobra.Command, f skewXYFlags) (float32, error) {
+
+	if cmd.Flags().Lookup(f.ratio.name).Changed {
+		return f.ratio.value, nil
+	}
+
+	if cmd.Flags().Lookup(f.degree.name).Changed {
+		rad := deg2rad(f.degree.value)
+		return rad2rat(rad), nil
+	}
+
+	if cmd.Flags().Lookup(f.radian.name).Changed {
+		return rad2rat(f.radian.value), nil
+	}
+
+	return 0, fmt.Errorf("must indicate a measure for skew correction: use --ratio --degree --radian flags")
+}
+
+func deg2rad(deg float32) float32 {
+	for deg > 360 {
+		deg -= 360
+	}
+	for deg < 0 {
+		deg += 360
+	}
+
+	rad := deg * math.Pi / 180
+
+	return rad
+}
+
+func rad2rat(rad float32) float32 {
+	rat := float32(math.Tan(float64(rad)))
+
+	return rat
 }
 
 // getInput return the correct input of gcode file as io.Reader instance according to arguments available
@@ -97,10 +152,6 @@ func getInput(stdio io.Reader, args []string) (io.Reader, error) {
 			return nil, fmt.Errorf("failed open file: %v", err)
 		}
 		input = file
-	}
-
-	if input == nil {
-		return nil, fmt.Errorf("input missing")
 	}
 
 	return input, nil
